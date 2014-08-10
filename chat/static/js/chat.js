c5445499c8b88220183e51b2f079e35b83029285
Anton Petrenko <petrenkoantserg@gmail.com>
2014-08-10 15:11:02 +0300

    simple ycombinator bot

--- chat/static/js/chat.js
+++ chat/static/js/chat.js
@@ -1,17 +1,67 @@
 $(document).ready(function() {
+    // Should bot post ycombinator messagess to this channel?
+    var tggl = false;
+
     var namespace = '/chat';
     var socket = io.connect('http://' + document.domain + ':' + location.port + namespace);
     var scrollDiv = document.getElementById("chat-messages");
+    var container = $('#chat-messages');
 
-    var new_message = function(usr, msg) {
-        msg = '<div class="message"><address>' + usr + '</address><span>' + msg + '</span></div>'
-        $('#chat-messages').append(msg);
+    var post_message = function(container, msg) {
+        container.append(msg);
         scrollDiv.scrollTop = scrollDiv.scrollHeight;
+    }
+
+    var user_message = function(usr, msg) {
+        msg = '<div class="message"><address>' + usr +
+              '</address><span>' + msg + '</span></div>'
+        return msg
     };
+
+    var system_message = function(msg) {
+        msg = '<p class=bg-info>' + msg + '</p>'
+        return msg
+    }
 
     var clear = function() {
         $('#new-message').val('');
     };
+
+    var parseRSS = function(url, container) {
+        $.ajax({
+            url: document.location.protocol +
+                 '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' +
+                 encodeURIComponent(url),
+            dataType: 'json',
+            success: function(data) {
+                new_message('YCOMBINATOR-BOT', '');
+                console.log(data);
+                $.each(data.responseData.feed.entries, function(key, value) {
+                    var content = '<p><a href="'+value.link+'" target="_blank">'+value.title+'</a></p>';
+                    $(container).append(content);
+                });
+            }
+        });
+    };
+
+    window.setInterval(function() {
+        if (tggl) {
+            parseRSS('https://news.ycombinator.com/rss', '#chat-messages');
+        }        
+    }, 180000);
+
+    $('#bot-btn').on('click', function() {
+        var $this = $(this);
+        tggl = !tggl;
+        if (tggl) {
+            post_message(container, system_message('Bot will be posting Ycombinator news every 3 minutes'));
+            $this.addClass('btn-success').removeClass('btn-warning');
+        } else {
+            $this.addClass('btn-warning').removeClass('btn-success');
+        }
+    });
+
+
 
     socket.on('connect', function() {
         var channelId = $('#chat').data('channelid');
@@ -23,7 +73,7 @@ $(document).ready(function() {
     });
 
     socket.on('msg_channel', function(user, msg) {
-        new_message(user, msg);
+        post_message(container, user_message(user, msg));
     });
 
 
@@ -31,7 +81,7 @@ $(document).ready(function() {
         e.preventDefault();
         var message = $('#new-message').val();
 
-        new_message('me', message);
+        post_message(container, user_message('me', message));
         clear();
 
         socket.emit('new message', message);
